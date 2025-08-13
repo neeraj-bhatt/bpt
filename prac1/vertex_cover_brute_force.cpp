e5c8e29a1ee88ef5cc37683a7cf5cbaebba27ae4
Neeraj Bhatt <95017273+neeraj-bhatt@users.noreply.github.com>
2025-08-13 13:55:00 +0530

    update to write running time to output file

--- prac1/vertex_cover_brute_force.cpp
+++ prac1/vertex_cover_brute_force.cpp
@@ -20,7 +20,7 @@ bool is_vertex_cover(const set<int>& vertex_cover, const vector<pair<int, int>>&
 }
 
 // Function to write result in ouput.txt file
-void generate_output_file(int &min_cover_size, set<int> best_cover){
+void generate_output_file(int &min_cover_size, set<int> best_cover, double time_taken){
     ofstream outfile("output.text");
     if (!outfile.is_open()) {
         cerr << "Error: Could not open output.txt for writing.\n";
@@ -31,11 +31,15 @@ void generate_output_file(int &min_cover_size, set<int> best_cover){
         outfile << vertex << " ";
     }
     outfile << endl;
+    outfile << "Running time: " << time_taken << " seconds" << endl;
     cout << "Results written to ouput file" << endl;
 }
 
 // Function to find the minimum vertex cover using brute force
 void brute_force_vertex_cover(int n, const vector<pair<int, int>>& edges) {
+    // start measuring time
+    clock_t start_time = clock();
+    
     int min_cover_size = n;  // The maximum size is n
     set<int> best_cover;
 
@@ -63,15 +67,13 @@ void brute_force_vertex_cover(int n, const vector<pair<int, int>>& edges) {
 
         if (min_cover_size == size) break;  // If we found the smallest vertex cover, stop
     }
+    
+    clock_t end_time = clock();
 
+    double time_taken = double(end_time - start_time) / CLOCKS_PER_SEC;
+    
     // Output the result
-    // cout << "Minimum vertex cover size: " << min_cover_size << endl;
-    // cout << "Best vertex cover: ";
-    // for (int vertex : best_cover) {
-    //     cout << vertex << " ";
-    // }
-    // cout << endl;
-    generate_output_file(min_cover_size, best_cover);
+    generate_output_file(min_cover_size, best_cover, time_taken);
 }
 
 // Function to read the graph from a file
@@ -95,19 +97,11 @@ int main() {
     // Read the graph from the file
     int n, m;
     vector<pair<int, int>> edges;
+    
     read_graph_from_file(filename, n, m, edges);
-
-    // Start measuring time
-    clock_t start_time = clock();
 
     // Find the minimum vertex cover using brute force
     brute_force_vertex_cover(n, edges);
-
-    // Measure running time
-    clock_t end_time = clock();
-    double time_taken = double(end_time - start_time) / CLOCKS_PER_SEC;
-    cout << "Running time: " << time_taken << " seconds" << endl;
-
     
     return 0;
 }
